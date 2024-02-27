f74bd4ff9008da3f1e5ff7ab4082e7d8a16d2556
Luis David Guaiquirian <luisgcode@gmail.com>
2024-02-26 21:54:41 -0800

    New contact button

--- javascript/script.js
+++ javascript/script.js
@@ -1,162 +1,133 @@
 "use strict";
 
-// counter increment
-const counterYears = document.querySelector(".counterYears");
-const counterProjects = document.querySelector(".counterProjects");
-const counterHours = document.querySelector(".counterHours");
+// // counter increment
+// const counterYears = document.querySelector(".counterYears");
+// const counterProjects = document.querySelector(".counterProjects");
+// const counterHours = document.querySelector(".counterHours");
 
-const incrementHours = function (counter) {
-  counterHours.innerText = "0";
+// const incrementHours = function (counter) {
+//   counterHours.innerText = "0";
 
-  const updateCounter = () => {
-    const target = +counterHours.getAttribute("data-target");
-    const c = +counterHours.innerText;
+//   const updateCounter = () => {
+//     const target = +counterHours.getAttribute("data-target");
+//     const c = +counterHours.innerText;
 
-    const increment = target / 300;
+//     const increment = target / 300;
 
-    if (c < target) {
-      counterHours.innerText = `${Math.ceil(c + increment)}`;
-      setTimeout(updateCounter, 1);
-    } else {
-      counterHours.innerText = target;
-    }
-  };
+//     if (c < target) {
+//       counterHours.innerText = `${Math.ceil(c + increment)}`;
+//       setTimeout(updateCounter, 1);
+//     } else {
+//       counterHours.innerText = target;
+//     }
+//   };
 
-  updateCounter();
-};
+//   updateCounter();
+// };
 
-const incrementProjects = function (counter) {
-  counterProjects.innerText = "0";
+// const incrementProjects = function (counter) {
+//   counterProjects.innerText = "0";
 
-  const updateCounter = () => {
-    const target = +counterProjects.getAttribute("data-target");
-    const c = +counterProjects.innerText;
+//   const updateCounter = () => {
+//     const target = +counterProjects.getAttribute("data-target");
+//     const c = +counterProjects.innerText;
 
-    const increment = target / 500;
+//     const increment = target / 500;
 
-    if (c < target) {
-      counterProjects.innerText = `${Math.ceil(c + increment)}`;
-      setTimeout(updateCounter, 90);
-    } else {
-      counterProjects.innerText = target;
-    }
-  };
+//     if (c < target) {
+//       counterProjects.innerText = `${Math.ceil(c + increment)}`;
+//       setTimeout(updateCounter, 90);
+//     } else {
+//       counterProjects.innerText = target;
+//     }
+//   };
 
-  updateCounter();
-};
+//   updateCounter();
+// };
 
-const incrementYears = function (counter) {
-  counterYears.innerText = "0";
+// const incrementYears = function (counter) {
+//   counterYears.innerText = "0";
 
-  const updateCounter = () => {
-    const target = +counterYears.getAttribute("data-target");
-    const c = +counterYears.innerText;
+//   const updateCounter = () => {
+//     const target = +counterYears.getAttribute("data-target");
+//     const c = +counterYears.innerText;
 
-    const increment = target / 500;
+//     const increment = target / 500;
 
-    if (c < target) {
-      counterYears.innerText = `${Math.ceil(c + increment)}`;
-      setTimeout(updateCounter, 600);
-    } else {
-      counterYears.innerText = target;
-    }
-  };
+//     if (c < target) {
+//       counterYears.innerText = `${Math.ceil(c + increment)}`;
+//       setTimeout(updateCounter, 600);
+//     } else {
+//       counterYears.innerText = target;
+//     }
+//   };
 
-  updateCounter();
-};
+//   updateCounter();
+// };
 
-const odometro = document.querySelector(".abilities-wrapper-counter");
-const odometroHeight = odometro.getBoundingClientRect().height;
+// const odometro = document.querySelector(".abilities-wrapper-counter");
+// const odometroHeight = odometro.getBoundingClientRect().height;
 
-function activarOdometro(entries) {
-  const entry = entries[0];
+// function activarOdometro(entries) {
+//   const entry = entries[0];
 
-  if (entry.isIntersecting) {
-    incrementHours();
-    incrementProjects();
-    incrementYears();
-  }
-}
-const observer = new IntersectionObserver(activarOdometro, {
-  rootMargin: `${odometroHeight + 10}px`,
-});
+//   if (entry.isIntersecting) {
+//     incrementHours();
+//     incrementProjects();
+//     incrementYears();
+//   }
+// }
+// const observer = new IntersectionObserver(activarOdometro, {
+//   rootMargin: `${odometroHeight + 10}px`,
+// });
 
-observer.observe(odometro);
+// observer.observe(odometro);
 
-// footer animation
-const menus = document.querySelectorAll(".top-icon-graph-options");
-const dots = document.querySelectorAll(".dots");
-const progressBar = document.querySelector(".circle-mark");
+// // Font sizes
+// const btnIncreaseFonts = document.querySelector(".toolbar-ul-li-increase-max");
+// const btnDecreaseFonts = document.querySelector(
+//   ".toolbar-ul-li-increase-regular"
+// );
 
-let currentValue = 1;
+// const regularText = document.querySelectorAll("p");
 
-let menusArray = Array.from(menus);
-let dotsArray = Array.from(dots);
+// btnIncreaseFonts.addEventListener("click", function () {
+//   regularText.forEach(function (text) {
+//     text.classList.add("agrandar");
+//   });
+// });
 
-menusArray.forEach(function (menu) {
-  menu.addEventListener("click", function () {
-    let idValue = menu.getAttribute("data-id");
+// btnDecreaseFonts.addEventListener("click", function () {
+//   regularText.forEach(function (text) {
+//     text.classList.remove("agrandar");
+//   });
+// });
 
-    if (idValue == 1) {
-      progressBar.style.height = "33%";
-    } else if (idValue == 2) {
-      progressBar.style.height = "63%";
-    } else if (idValue == 3) {
-      progressBar.style.height = "100%";
-    }
+// // Scroll Revealing //
+// const allRevealSections = document.querySelectorAll(".scroll");
 
-    progressBar.style.width = menusArray.forEach(function (men) {
-      men.classList.remove("active");
-    });
+// const revealSection = function (entries, oberserver) {
+//   const [entry] = entries;
 
-    dotsArray.forEach(function (dot) {
-      dot.classList.remove("active");
-    });
+//   if (!entry.isIntersecting) return;
+//   entry.target.classList.remove("scroll-hidden");
+//   oberserver.unobserve(entry.target);
+// };
 
-    this.classList.add("active");
+// const sectionOberserver = new IntersectionObserver(revealSection, {
+//   root: null,
+//   threshold: 0.1,
+// });
 
-    var dotsIndex = menusArray.indexOf(this);
-    dotsArray[dotsIndex + 1].classList.add("active");
-  });
-});
+// allRevealSections.forEach(function (section) {
+//   sectionOberserver.observe(section);
+//   section.classList.add("scroll-hidden");
+// });
 
-// Font sizes
-const btnIncreaseFonts = document.querySelector(".toolbar-ul-li-increase-max");
-const btnDecreaseFonts = document.querySelector(
-  ".toolbar-ul-li-increase-regular"
-);
+// Dropdown menu
+const dropDownActivator = document.querySelector(".dropActivator");
+const dropdown = document.querySelector(".dropdown");
 
-const regularText = document.querySelectorAll("p");
-
-btnIncreaseFonts.addEventListener("click", function () {
-  regularText.forEach(function (text) {
-    text.classList.add("agrandar");
-  });
-});
-
-btnDecreaseFonts.addEventListener("click", function () {
-  regularText.forEach(function (text) {
-    text.classList.remove("agrandar");
-  });
-});
-
-// Scroll Revealing //
-const allRevealSections = document.querySelectorAll(".scroll");
-
-const revealSection = function (entries, oberserver) {
-  const [entry] = entries;
-
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove("scroll-hidden");
-  oberserver.unobserve(entry.target);
-};
-
-const sectionOberserver = new IntersectionObserver(revealSection, {
-  root: null,
-  threshold: 0.1,
-});
-
-allRevealSections.forEach(function (section) {
-  sectionOberserver.observe(section);
-  section.classList.add("scroll-hidden");
+dropDownActivator.addEventListener("click", function () {
+  dropdown.classList.toggle("dropdownActivated");
 });
