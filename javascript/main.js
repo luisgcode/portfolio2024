8649eaa98f3caa3e3b8e1ab066e40658e5ca9bf9
luisgcode <luisgcode@gmail.com>
2024-04-28 12:36:23 -0400

    js minify file

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,5 @@
 "use strict";
-// main.js
 document.addEventListener("DOMContentLoaded", function () {
-  commonFunction();
   // // counter increment
   const counterYears = document.querySelector(".counterYears");
   const counterProjects = document.querySelector(".counterProjects");
@@ -84,4 +82,107 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   observer.observe(odometro);
+});
+
+// // Hamburguer menu displaying
+const openNavBtns = document.querySelectorAll(".toolbar-mobile-ul-hamburguer");
+const mobileMenus = document.querySelectorAll(".nav-mobile");
+
+openNavBtns.forEach(function (openBtn) {
+  openBtn.addEventListener("click", function () {
+    mobileMenus.forEach(function (mobileMenu) {
+      mobileMenu.classList.toggle("active");
+    });
+  });
+});
+
+// Print button function
+const printLinks = document.querySelectorAll('[id^="printLink"]');
+
+printLinks.forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.print();
+  });
+});
+
+// contact modal
+const modal = document.querySelector(".contact");
+const btnOpenModal = document.querySelectorAll(".open-modal");
+const btnCloseModal = document.querySelectorAll(".close-modal");
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+};
+
+btnOpenModal.forEach(function (btn) {
+  btn.addEventListener("click", openModal);
+});
+
+btnCloseModal.forEach(function (btn) {
+  btn.addEventListener("click", closeModal);
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+// Font sizes
+const btnIncreaseFonts = document.querySelectorAll(".btn-increase");
+const regularText = document.querySelectorAll("p");
+
+const increaseSizeFont = function () {
+  btnIncreaseFonts.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      regularText.forEach(function (text) {
+        text.classList.toggle("increase");
+      });
+    });
+  });
+};
+
+increaseSizeFont();
+
+// // Scroll Revealing //
+const allRevealSections = document.querySelectorAll(".scroll");
+
+const revealSection = function (entries, oberserver) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("scroll-hidden");
+  oberserver.unobserve(entry.target);
+};
+
+const sectionOberserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.1,
+});
+
+allRevealSections.forEach(function (section) {
+  sectionOberserver.observe(section);
+  section.classList.add("scroll-hidden");
+});
+
+// play videos with hover
+const btnVideos = document.querySelectorAll(".videoBtn");
+const videosCard = document.querySelectorAll("projects-card");
+
+btnVideos.forEach(function (video) {
+  video.addEventListener("mouseover", function () {
+    video.play();
+  });
+});
+
+btnVideos.forEach(function (video) {
+  video.addEventListener("mouseout", function () {
+    video.pause();
+  });
 });
